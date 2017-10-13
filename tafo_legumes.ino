270db1be03ca21913b0db22875c865da7e6645df
Jonathan Perret <jonathan@ut7.fr>
2017-10-13 10:40:08 +0200

    Config in EEPROM, serial interface (doesn't completely work)

--- tafo_legumes.ino
+++ tafo_legumes.ino
@@ -1,21 +1,5 @@
-/*********************************************************
-This is a library for the MPR121 12-channel Capacitive touch sensor
-
-Designed specifically to work with the MPR121 Breakout in the Adafruit shop 
-  ----> https://www.adafruit.com/products/
-
-These sensors use I2C communicate, at least 2 pins are required 
-to interface
-
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
-products from Adafruit!
-
-Written by Limor Fried/Ladyada for Adafruit Industries.  
-BSD license, all text above must be included in any redistribution
-**********************************************************/
-
 #include <Wire.h>
+#include <EEPROM.h>
 #include "Adafruit_MPR121.h"
 #include <Adafruit_NeoPixel.h>
 
@@ -25,6 +9,8 @@ BSD license, all text above must be included in any redistribution
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      8
+
+#define NUMLIGHTS      2
 
 #define NUMELECTRODES 12
 
@@ -57,51 +43,80 @@ Adafruit_MPR121 cap = Adafruit_MPR121();
 uint16_t lasttouched = 0;
 uint16_t currtouched = 0;
 
-uint32_t targetColors1[NUMELECTRODES] = {
-  Adafruit_NeoPixel::Color(255,255,255,255),
-  Adafruit_NeoPixel::Color(0,0,0,0),
-  Adafruit_NeoPixel::Color(255,255,0,0),
-  Adafruit_NeoPixel::Color(0,0,0,0),
-  
-  Adafruit_NeoPixel::Color(255,255,0,0),
-  Adafruit_NeoPixel::Color(255,0,0,0),
-  Adafruit_NeoPixel::Color(255,255,255,255),
-  Adafruit_NeoPixel::Color(0,0,255,0),
-  
-  Adafruit_NeoPixel::Color(255,255,0,0),
-  Adafruit_NeoPixel::Color(255,0,0,0),
-  Adafruit_NeoPixel::Color(0,0,0,0),
-  Adafruit_NeoPixel::Color(0,0,255,0),
-};
+#define VERSION 0x00000001
 
-uint32_t targetColors2[NUMELECTRODES] = {
-  Adafruit_NeoPixel::Color(0,0,0,0),
-  Adafruit_NeoPixel::Color(255,255,255,255),
-  Adafruit_NeoPixel::Color(0,0,0,0),
-  Adafruit_NeoPixel::Color(255,255,0,0),
-  
-  Adafruit_NeoPixel::Color(255,255,0,0),
-  Adafruit_NeoPixel::Color(255,0,0,0),
-  Adafruit_NeoPixel::Color(255,255,255,255),
-  Adafruit_NeoPixel::Color(255,255,255,255),
-  
-  Adafruit_NeoPixel::Color(255,0,0,0),
-  Adafruit_NeoPixel::Color(0,0,0,0),
-  Adafruit_NeoPixel::Color(255,0,0,0),
-  Adafruit_NeoPixel::Color(0,0,0,0),
+struct {
+  uint32_t version;
+  uint32_t targetColors[NUMLIGHTS][NUMELECTRODES];
+} config = {
+  VERSION,
+  {
+    {
+      Adafruit_NeoPixel::Color(255,255,255,255),
+      Adafruit_NeoPixel::Color(0,0,0,0),
+      Adafruit_NeoPixel::Color(255,255,0,0),
+      Adafruit_NeoPixel::Color(0,0,0,0),
+
+      Adafruit_NeoPixel::Color(255,255,0,0),
+      Adafruit_NeoPixel::Color(255,0,0,0),
+      Adafruit_NeoPixel::Color(255,255,255,255),
+      Adafruit_NeoPixel::Color(0,0,255,0),
+
+      Adafruit_NeoPixel::Color(255,255,0,0),
+      Adafruit_NeoPixel::Color(255,0,0,0),
+      Adafruit_NeoPixel::Color(0,0,0,0),
+      Adafruit_NeoPixel::Color(0,0,255,0),
+    },
+    {
+      Adafruit_NeoPixel::Color(0,0,0,0),
+      Adafruit_NeoPixel::Color(255,255,255,255),
+      Adafruit_NeoPixel::Color(0,0,0,0),
+      Adafruit_NeoPixel::Color(255,255,0,0),
+
+      Adafruit_NeoPixel::Color(255,255,0,0),
+      Adafruit_NeoPixel::Color(255,0,0,0),
+      Adafruit_NeoPixel::Color(255,255,255,255),
+      Adafruit_NeoPixel::Color(255,255,255,255),
+
+      Adafruit_NeoPixel::Color(255,0,0,0),
+      Adafruit_NeoPixel::Color(0,0,0,0),
+      Adafruit_NeoPixel::Color(255,0,0,0),
+      Adafruit_NeoPixel::Color(0,0,0,0),
+    }
+  }
 };
 
 uint32_t saturation[NUMELECTRODES] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
- 
+
+void loadConfig() {
+  uint32_t version;
+  EEPROM.get(0, version);
+  if (version == VERSION) {
+    Serial.println("Found config in EEPROM, loading it");
+    EEPROM.get(0, config);
+  }
+}
+
+void saveConfig() {
+  EEPROM.put(0, config);
+}
+
+#define HAS_MPR121 1
+
 void setup() {
   Serial.begin(115200);
 
   while (!Serial) { // needed to keep leonardo/micro from starting too fast!
     delay(10);
   }
-  
-  Serial.println("Adafruit MPR121 Capacitive Touch sensor test"); 
-  
+
+  Serial.println("Starting up...");
+
+  loadConfig();
+
+#if HAS_MPR121
+  Serial.println("Looking for MPR121");
+
   // Default address is 0x5A, if tied to 3.3V its 0x5B
   // If tied to SDA its 0x5C and if SCL then 0x5D
   if (!cap.begin(0x5A)) {
@@ -109,11 +124,12 @@ void setup() {
     while (1);
   }
   Serial.println("MPR121 found!");
-
-  //cap.setThresholds(12,0);
+#endif
 
   pixels1.begin();
   pixels2.begin();
+
+  Serial.println("Ready");
 }
 
 void change(int electrodeIndex, bool direction) {
@@ -150,15 +166,6 @@ void show(Adafruit_NeoPixel& pixels, uint32_t *targetColors) {
   sumb = min(255, sumb >> 8);
   sumw = min(255, sumw >> 8);
 
-  Serial.print(sumr);
-  Serial.print("\t");
-  Serial.print(sumg);
-  Serial.print("\t");
-  Serial.print(sumb);
-  Serial.print("\t");
-  Serial.print(sumw);
-  Serial.print("\t");
-
   for (uint8_t i=0; i<NUMPIXELS; i++) {
     pixels.setPixelColor(i, neopix_gamma[sumr], neopix_gamma[sumg], neopix_gamma[sumb], neopix_gamma[sumw]);
   }
@@ -166,39 +173,124 @@ void show(Adafruit_NeoPixel& pixels, uint32_t *targetColors) {
   pixels.show();
 }
 
+void processSerialInput() {
+  // if there's any serial available, read it:
+  while (Serial.available() > 0) {
+    char cmd = Serial.read();
+
+    switch(cmd) {
+      case 'Q': {
+        Serial.print("OK ");
+        Serial.print(NUMLIGHTS);
+        Serial.print(" ");
+        Serial.println(NUMELECTRODES);
+        for(int numLight = 0; numLight < NUMLIGHTS; numLight++) {
+          for(int numElectrode = 0; numElectrode < NUMELECTRODES; numElectrode++) {
+            Serial.print(numLight);
+            Serial.print(" ");
+
+            Serial.print(numElectrode);
+            Serial.print(" ");
+
+            uint32_t targetColor = config.targetColors[numLight][numElectrode];
+
+            uint32_t w = (uint8_t)(targetColor >> 24),
+                     r = (uint8_t)(targetColor >> 16),
+                     g = (uint8_t)(targetColor >>  8),
+                     b = (uint8_t)targetColor;
+
+            Serial.print(r);
+            Serial.print(" ");
+            Serial.print(g);
+            Serial.print(" ");
+            Serial.print(b);
+            Serial.print(" ");
+            Serial.println(w);
+          }
+        }
+        break;
+      }
+      case 'C': {
+        int numLight = Serial.parseInt();
+        int numElectrode = Serial.parseInt();
+        int red = Serial.parseInt();
+        int green = Serial.parseInt();
+        int blue = Serial.parseInt();
+        int white = Serial.parseInt();
+        if(numLight < 0 || numLight >= NUMLIGHTS) {
+          Serial.print("ERR invalid light ");
+          Serial.println(numLight);
+          break;
+        }
+        if(numElectrode < 0 || numElectrode >= NUMELECTRODES) {
+          Serial.print("ERR invalid electrode ");
+          Serial.println(numElectrode);
+          break;
+        }
+        Serial.print("OK Setting light ");
+        Serial.print(numLight);
+        Serial.print(" for electrode ");
+        Serial.print(numElectrode);
+        Serial.print(" to (");
+        Serial.print(red);
+        Serial.print(",");
+        Serial.print(green);
+        Serial.print(",");
+        Serial.print(blue);
+        Serial.print(",");
+        Serial.print(white);
+        Serial.println(")");
+        config.targetColors[numLight][numElectrode] = Adafruit_NeoPixel::Color(red, green, blue, white);
+        saveConfig();
+        break;
+      }
+      case '\r':
+      case '\n':
+        break;
+      default:
+        Serial.print("ERR Unknown command ");
+        Serial.println(cmd, 16);
+        break;
+    }
+  }
+}
+
 void loop() {
+#if HAS_MPR121
   // Get the currently touched pads
   currtouched = cap.touched();
-  
+
   for (uint8_t i=0; i<NUMELECTRODES; i++) {
     // it if *is* touched and *wasnt* touched before, alert!
     if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
-      Serial.print(i); Serial.println(" touched");
+      //Serial.print(i); Serial.println(" touched");
     }
     // if it *was* touched and now *isnt*, alert!
     if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
-      Serial.print(i); Serial.println(" released");
+      //Serial.print(i); Serial.println(" released");
     }
   }
 
   for (uint8_t electrodeIndex=0; electrodeIndex<NUMELECTRODES; electrodeIndex++) {
     change(electrodeIndex, currtouched & _BV(electrodeIndex));
 
-    Serial.print(saturation[electrodeIndex]);
-    Serial.print("\t");
+    //Serial.print(saturation[electrodeIndex]);
+    //Serial.print("\t");
   }
+  //Serial.println();
+#endif
 
-  show(pixels1, targetColors1);
-  show(pixels2, targetColors2);
+  show(pixels1, config.targetColors[0]);
+  show(pixels2, config.targetColors[1]);
 
-  Serial.println();
+  processSerialInput();
 
   // reset our state
   lasttouched = currtouched;
 
   // comment out this line for detailed data from the sensor!
   return;
-  
+
   // debugging info, what
   Serial.print("\t\t\t\t\t\t\t\t\t\t\t\t\t 0x"); Serial.println(cap.touched(), HEX);
   Serial.print("Filt: ");
@@ -211,7 +303,7 @@ void loop() {
     Serial.print(cap.baselineData(i)); Serial.print("\t");
   }
   Serial.println();
-  
+
   // put a delay so it isn't overwhelming
   delay(100);
 }
